642f9bc87c1c8766636877abf45f160705e71f01
Tacopocalyptic <130405272+Tacopocalyptic@users.noreply.github.com>
2024-12-08 21:37:11 -0500

    different attempt at in-app directions

--- components/DirectionsMap.js
+++ components/DirectionsMap.js
@@ -1,8 +1,13 @@
-import { View, Text, StyleSheet, PermissionsAndroid } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import MapView from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
 import { useEffect, useState } from "react";
+import * as Location from 'expo-location';
 const GOOGLE_MAPS_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+
+// === CURRENTLY UNUSED, DIRECTIONS BUTTON OPENS RELEVANT MAP APP ===
+
 
 export default function DirectionsMap({ route }) {
   const { region, restaurant } = route.params;
@@ -11,11 +16,21 @@ export default function DirectionsMap({ route }) {
 
   useEffect(() => {
     // Get permissions, then get location
+    getPermissionAndLocation();
   },[])
 
+  const getPermissionAndLocation = async() => {
+    let {status} = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted'){
+      console.log("Please grant location permissions")
+      return
+    }
+    let currLocation = await Location.getCurrentPositionAsync({});
+    console.log(currLocation)
+    setLocation(currLocation.coords)
+  }
 
-
-  return !currentLocation ? (
+  return !location ? (
     <Text>Getting your location...</Text> 
   ) : (
     <View style={styles.container}>
@@ -24,6 +39,11 @@ export default function DirectionsMap({ route }) {
         initialRegion={region}
         key={region.latitude + region.longitude}
       >
+        <Marker
+          coordinate={region}
+          title={restaurant.name}
+          description={restaurant.address.street}
+        />
         <MapViewDirections 
           origin={location}
           destination={region}
